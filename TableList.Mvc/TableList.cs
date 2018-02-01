599402fa781778b5a2b679003c84ddd42d8df971
Hovhannes Bantikyan <bantikyan@gmail.com>
2018-02-02 03:00:28 +0400

    Init

---
+++ TableList.Mvc/TableList.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Zetalex.TableList.Mvc
+{
+    public static class TableList
+    {
+        public static MvcHtmlString TableListFor<TModel, TProperty>(this HtmlHelper<TModel> html, Expression<Func<TModel, TProperty>> expression, bool showHeader = false, bool allowAdd = true)
+        {
+            return TableListFor(html, expression, null, showHeader: showHeader, allowAdd: allowAdd);
+        }
+
+        public static MvcHtmlString TableListFor<TModel, TProperty>(this HtmlHelper<TModel> html, Expression<Func<TModel, TProperty>> expression, object htmlAttributes, bool showHeader = false, bool allowAdd = true)
+        {
+            return TableListFor(html, expression, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes), showHeader: showHeader, allowAdd: allowAdd);
+        }
+
+        public static MvcHtmlString TableListFor<TModel, TProperty>(this HtmlHelper<TModel> html, Expression<Func<TModel, TProperty>> expression, IDictionary<string, object> htmlAttributes, bool showHeader = false, bool allowAdd = true)
+        {
+            var fieldName = ExpressionHelper.GetExpressionText(expression);
+            //var fullBindingName = html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(fieldName);
+            //var fieldId = TagBuilder.CreateSanitizedId(fullBindingName);
+
+            var metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+            dynamic model = metadata.Model;
+            Type baseType = metadata.ModelType.GetGenericArguments().Single();
+
+            var properties = baseType.GetProperties().ToList();
+            var typeMetadata = ModelMetadataProviders.Current.GetMetadataForType(null, baseType);
+
+            Dictionary<string, IDictionary<string, object>> propertyAttributes = new Dictionary<string, IDictionary<string, object>>();
+            Dictionary<string, Dictionary<string, string>> formattingAttributes = new Dictionary<string, Dictionary<string, string>>();
+
+            foreach (var prop in properties)
+            {
+                var typeName = GetPropertyTypeName(prop);
+
+                var propertyData = typeMetadata.Properties.Where(x => x.PropertyName == prop.Name).FirstOrDefault();
+                var attr = html.GetUnobtrusiveValidationAttributes(prop.Name, propertyData);
+                attr.Add("class", "form-control");
+                attr.Add("placeholder", propertyData.DisplayName ?? prop.Name);
+                if (propertyData.IsReadOnly)
+                {
+                    attr.Add("readonly", "readonly");
+                }
+
+                if (typeName == "DateTime")
+                {
+                    attr["class"] += " date-picker";
+                }
+
+                propertyAttributes.Add(prop.Name, attr);
+
+                Dictionary<string, string> fAttr = new Dictionary<string, string>();
+                fAttr.Add(nameof(propertyData.DisplayName), propertyData.DisplayName);
+                fAttr.Add(nameof(propertyData.DisplayFormatString), propertyData.DisplayFormatString);
+                formattingAttributes.Add(prop.Name, fAttr);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            if (showHeader)
+            {
+                BuildTableHeader(sb, properties, formattingAttributes);
+            }
+
+            sb.AppendLine("<tbody>");
+
+            if (model != null && model.Count > 0)
+            {
+                BuildTableRows(html, sb, properties, model, fieldName, propertyAttributes, formattingAttributes);
+            }
+
+            if (allowAdd)
+            {
+                BuildLastTableRow(html, sb, properties, fieldName, propertyAttributes, formattingAttributes, model != null ? model.Count : 0);
+            }
+
+            sb.AppendLine("</tbody>");
+
+            TagBuilder tag = new TagBuilder("table");
+            if (htmlAttributes != null)
+            {
+                tag.MergeAttributes(htmlAttributes);
+            }
+
+            if (tag.Attributes.ContainsKey("class"))
+            {
+                tag.Attributes["class"] += " table-list-mvc";
+            }
+            else
+            {
+                tag.Attributes.Add("class", "table-list-mvc");
+            }
+
+            tag.InnerHtml = sb.ToString();
+
+            return new MvcHtmlString(tag.ToString(TagRenderMode.Normal));
+        }
+
+        private static void BuildTableRows(HtmlHelper html, StringBuilder sb, List<PropertyInfo> properties, dynamic items, string fieldName, Dictionary<string, IDictionary<string, object>> propertyAttributes, Dictionary<string, Dictionary<string, string>> formattingAttributes)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                var fName = fieldName + "[" + i + "].";
+
+                sb.AppendLine("<tr class=\"table-list-mvc-item-view\" data-item-index=\"" + i + "\">");
+
+                var allowModify = (bool)properties.FirstOrDefault(p => p.Name.ToLower() == nameof(TableListItem.TL_AllowModify).ToLower()).GetValue((TableListItem)items[i]);
+
+                foreach (var prop in properties)
+                {
+                    var isHidden = Attribute.IsDefined(prop, typeof(TableListHiddenInput));
+                    if (isHidden)
+                    {
+                        continue;
+                    }
+
+                    var propertyAttributesClone = CloneDictionary(propertyAttributes);
+                    if (!allowModify && !propertyAttributesClone[prop.Name].ContainsKey("readonly"))
+                    {
+                        propertyAttributesClone[prop.Name].Add("readonly", "readonly");
+                    }
+
+                    sb.Append("<td>");
+                    sb.Append(html.TextBox(fName + prop.Name, prop.GetValue((TableListItem)items[i]), formattingAttributes[prop.Name]["DisplayFormatString"], propertyAttributesClone[prop.Name]));
+                    sb.Append(html.ValidationMessage(fName + prop.Name));
+                    sb.AppendLine("</td>");
+                }
+
+                sb.Append("<td>");
+
+                foreach (var prop in properties)
+                {
+                    var isHidden = Attribute.IsDefined(prop, typeof(TableListHiddenInput));
+                    if (!isHidden)
+                    {
+                        continue;
+                    }
+
+                    var val = prop.GetValue((TableListItem)items[i]);
+                    sb.Append(html.Hidden(fName + prop.Name, val));
+                    if (prop.Name == nameof(TableListItem.TL_AllowDelete) && (bool)val)
+                    {
+                        sb.Append("<a href=\"#\" class=\"table-list-mvc-item-delete\">Delete</a>");
+                    }
+                }
+
+                sb.AppendLine("</td>");
+
+                sb.AppendLine("</tr>");
+            }
+        }
+
+        private static void BuildLastTableRow(HtmlHelper html, StringBuilder sb, List<PropertyInfo> properties, string fieldName, Dictionary<string, IDictionary<string, object>> propertyAttributes, Dictionary<string, Dictionary<string, string>> formattingAttributes, int index)
+        {
+            var fName = fieldName + "[" + index + "].";
+
+            sb.AppendLine("<tr class=\"table-list-mvc-item-new\" data-item-index=\"" + index + "\">");
+
+            foreach (var prop in properties)
+            {
+                var isHidden = Attribute.IsDefined(prop, typeof(TableListHiddenInput));
+                if (isHidden)
+                {
+                    continue;
+                }
+
+                //if (propertyAttributes[prop.Name].ContainsKey("readonly"))
+                //{
+                //    propertyAttributes[prop.Name].Remove("readonly");
+                //}
+
+                //TODO FIX
+                propertyAttributes[prop.Name]["class"] += " table-list-mvc-ignore";
+
+                sb.Append("<td>");
+                sb.Append(html.TextBox(fName + prop.Name, null, formattingAttributes[prop.Name]["DisplayFormatString"], propertyAttributes[prop.Name]));
+                //sb.Append(html.CheckBox(fName + prop.Name, false, propertyAttributes[prop.Name]));
+                sb.Append(html.ValidationMessage(fName + prop.Name));
+                sb.AppendLine("</td>");
+            }
+
+            sb.Append("<td>");
+
+            foreach (var prop in properties)
+            {
+                var isHidden = Attribute.IsDefined(prop, typeof(TableListHiddenInput));
+                if (!isHidden)
+                {
+                    continue;
+                }
+
+                if (prop.Name == nameof(TableListItem.TL_State))
+                {
+                    sb.Append(html.Hidden(fName + prop.Name, TableListItemState.Added));
+                }
+                else if (prop.Name == nameof(TableListItem.TL_AllowModify))
+                {
+                    sb.Append(html.Hidden(fName + prop.Name, true));
+                }
+                else if (prop.Name == nameof(TableListItem.TL_AllowDelete))
+                {
+                    sb.Append(html.Hidden(fName + prop.Name, true));
+                    sb.Append("<a href=\"#\" class=\"table-list-mvc-item-delete\">Delete</a>");
+                }
+                else
+                {
+                    sb.Append(html.Hidden(fName + prop.Name));
+                }
+            }
+
+            sb.AppendLine("</td>");
+            sb.AppendLine("</tr>");
+        }
+
+        private static void BuildTableHeader(StringBuilder sb, List<PropertyInfo> properties, Dictionary<string, Dictionary<string, string>> formattingAttributes)
+        {
+            sb.AppendLine("<thead>");
+            sb.AppendLine("<tr>");
+
+            foreach (var prop in properties)
+            {
+                var isHidden = Attribute.IsDefined(prop, typeof(TableListHiddenInput));
+                if (isHidden)
+                {
+                    continue;
+                }
+
+                sb.Append("<th>");
+                sb.Append(formattingAttributes[prop.Name]["DisplayName"] ?? prop.Name);
+                sb.AppendLine("</th>");
+            }
+
+            sb.AppendLine("<th></th>");
+
+            sb.AppendLine("</tr>");
+            sb.AppendLine("</thead>");
+        }
+
+        private static Dictionary<string, IDictionary<string, object>> CloneDictionary(Dictionary<string, IDictionary<string, object>> propertyAttributes)
+        {
+            Dictionary<string, IDictionary<string, object>> propertyAttributesClone = new Dictionary<string, IDictionary<string, object>>();
+            foreach (var prop in propertyAttributes)
+            {
+                propertyAttributesClone.Add(prop.Key, new Dictionary<string, object>());
+                foreach (var item in prop.Value)
+                {
+                    propertyAttributesClone[prop.Key].Add(new KeyValuePair<string, object>(item.Key, item.Value));
+                }
+            }
+
+            return propertyAttributesClone;
+        }
+
+        private static string GetPropertyTypeName(PropertyInfo prop)
+        {
+            string typeName = "";
+            if (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                typeName = prop.PropertyType.GetGenericArguments()[0].Name;
+            }
+            else
+            {
+                typeName = prop.PropertyType.Name;
+            }
+
+            return typeName;
+        }
+    }
+}
