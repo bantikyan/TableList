16ffa70f8f144b5b3ca7ad999685275c01332acd
bantikyan <bantikyan@gmail.com>
2018-02-02 11:46:46 +0400

    Fix

--- examples/TableList.Example.Mvc/Controllers/HomeController.cs
+++ examples/TableList.Example.Mvc/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Zetalex.TableList.Example.Mvc.Models;
 
 namespace TableList.Example.Mvc.Controllers
 {
@@ -10,7 +11,23 @@ namespace TableList.Example.Mvc.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var model = new ProductModel
+            {
+                ID = 1,
+                Initial = (decimal)547,
+                BlockPrices = new List<ProductBlockPrice>
+                {
+                    new ProductBlockPrice { Initial = 600, CountFrom = 30 }
+                }
+                //Options = new List<InnerModel>
+                //             {
+                //                 new InnerModel { ID = 1, FirstName = "item1", LastName = "last1", TL_AllowDelete = false },
+                //                 new InnerModel { ID = 2, FirstName = "item2", LastName = "last2", TL_AllowModify = false },
+                //                 new InnerModel { ID = 3, FirstName = "item3", LastName = "last3", BirthDate = new DateTime(1988, 11, 30) },
+                //             },
+            };
+
+            return View(model);
         }
 
         public ActionResult About()
---
+++ examples/TableList.Example.Mvc/Models/ProductModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zetalex.TableList.Example.Mvc.Models
+{
+    [Serializable]
+    public class ProductModel
+    {
+        public int ID { get; set; }
+        public int WhiteLabelID { get; set; }
+
+        [Required]
+        [AllowHtml]
+        public string Title { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
+        public decimal Initial { get; set; }
+
+        public List<ProductBlockPrice> BlockPrices { get; set; }
+    }
+
+    [Serializable]
+    public class ProductBlockPrice : TableListItem
+    {
+        [TableListHiddenInput]
+        public int ID { get; set; }
+
+        [TableListHiddenInput]
+        public int ProductID { get; set; }
+        //[Required]
+        //[Range(0, Double.MaxValue, ErrorMessage = "Value must be greater than or equal to 0")]
+        //[Display(Name = "Price")]
+        //[DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
+        //public decimal Price { get; set; }
+
+        //[Range(0, Double.MaxValue, ErrorMessage = "Value must be greater than or equal to 0")]
+        //[Display(Name = "Renewal Price")]
+        //[DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
+        //public decimal? RenewalPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
+        public decimal Initial { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
+        public decimal? Renewal { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "License Count must be greater then 0")]
+        public int CountFrom { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "License Count must be greater then 0")]
+        [DisplayName("Count To")]
+        public int? CountTo { get; set; }
+    }
+}
