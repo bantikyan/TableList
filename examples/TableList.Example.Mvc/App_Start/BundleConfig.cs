cf0804a7c650b440a727447793d6cee12cbbb52b
bantikyan <bantikyan@gmail.com>
2018-02-02 12:03:16 +0400

    Fix

--- examples/TableList.Example.Mvc/App_Start/BundleConfig.cs
+++ examples/TableList.Example.Mvc/App_Start/BundleConfig.cs
@@ -21,11 +21,13 @@ namespace TableList.Example.Mvc
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/tablelist-mvc.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/tablelist-mvc.css"));
         }
     }
 }
