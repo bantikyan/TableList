89e4d8ce9cedb43ef9e444f9e03ac61a1c74cffb
Hovhannes Bantikyan <bantikyan@gmail.com>
2018-02-20 01:32:09 +0400

    Versioning

--- src/TableList.Mvc/Properties/AssemblyInfo.cs
+++ src/TableList.Mvc/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TableList.Mvc")]
-[assembly: AssemblyDescription("Table List Description")]
+[assembly: AssemblyDescription("Editor template for inline editing list of items. ASP.NET MVC version.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hovhannes Bantikyan, Vahe Khachaturyan")]
 [assembly: AssemblyProduct("TableList.Mvc")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
